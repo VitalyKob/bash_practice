f02dd46ee42fe894e0277d48b58dd5735d7d3a8b
Vitaly <VitalyKob@users.noreply.github.com>
2018-01-10 14:01:51 -0500

    Associative array

---
+++ instruments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -A beatles
+
+beatles=(
+
+[singer]=John
+[bassist]=Paul
+[drummer]=Ringo
+[guitarist]=George
+
+)
+
+for instrument in ${!beatles[@]}
+do
+  echo "The ${instrument} is ${beatles[$instrument]}"
+done
